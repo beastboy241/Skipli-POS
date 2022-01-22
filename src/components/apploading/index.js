f257073b4269f489ff99b66ca774f42f31415431
tedymaradho <maradho@gmail.com>
2022-01-23 06:33:55 +0700

    add registration and apploading

---
+++ src/components/apploading/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import LinierProgress from '@material-ui/core/LinearProgress';
+import useStyles from './styles';
+
+function AppLoading(){
+    const classes = useStyles();
+
+    return (
+        <Container maxWidth="xs">
+            <div className={classes.loadingBox}>
+                <Typography
+                    variant="h6"
+                    component="h2"
+                    className={classes.title}
+                >
+                    MaraPOS
+                </Typography>
+                <LinierProgress />
+            </div>
+        </Container>
+    )
+}
+
+export default AppLoading;
