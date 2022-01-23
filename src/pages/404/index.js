8f8ec55da63aca6575a1b2a6e0d31b4e7b5c4f49
tedymaradho <maradho@gmail.com>
2022-01-24 06:21:11 +0700

    add not found page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from "@material-ui/core/Typography";
+import {Link} from 'react-router-dom';
+import useStyles from './styles';
 
 const NotFound = () => {
-    return <h1>Halaman tidak ditemukan</h1>
+    const classes = useStyles();
+    return (
+        <Container maxWidht="xs">
+            <Paper className={classes.paper}>
+                <Typography variant="subtitle1">
+                    Halaman tidak ditemukan
+                </Typography>
+                <Typography variant="h3">
+                    404
+                </Typography>
+                <Typography component={Link} to="/">
+                    Kembali ke Beranda
+                </Typography>
+            </Paper>
+        </Container>
+    )
 }
 
 export default NotFound;
